4bddcc3db469e5865471c566692f5dddf4bc5356
Shriphani Palakodety <shriphanip@gmail.com>
2013-08-09 00:21:07 -0400

    gaia online scraper added. need to add clueweb12++ crawler header

---
+++ clojure/clueweb12pp_core/src/clueweb12pp_core/gaia/gaia_posts_scraper.clj
@@ -0,0 +1,62 @@
+;;;; Scrape GAIA Online post pages
+
+(ns clueweb12pp-core.gaia.gaia_posts_scraper
+  (:gen-class :main true)
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clojure.tools.cli :as cli]
+            [clueweb12pp-core.core :as core]
+            [net.cgrand.enlive-html :as html]
+            [org.bovinegenius.exploding-fish :as uri])
+  (:import java.util.zip.GZIPOutputStream))
+
+(defn download
+  "Downloads and returns the page html content"
+  [page-uri]
+  (when page-uri
+    (:body (client/get page-uri))))
+
+(defn next-page
+  "Follows the next link on a post page"
+  [page-content]
+  (when page-content
+    (-> (first
+        (html/select
+         (html/html-resource
+          (java.io.StringReader. page-content))
+         [:a.page_jump]))
+
+       :attrs
+       :href)))
+
+(defn dump-page
+  [page-uri page-content output-file]
+  (with-open [output (-> output-file
+                        clojure.java.io/output-stream
+                        GZIPOutputStream.
+                        clojure.java.io/writer)]
+    (binding [*out* output]
+      (println
+       (json/write-str
+        {:uri     page-uri
+         :content page-content})))))
+
+(defn download-post
+  "Starts from the post seed and downloads the entire page"
+  [post-seed output-file]
+  (let [post-seed-page (download post-seed)]
+    (do
+      (println post-seed)
+      (flush)
+      (dump-page post-seed post-seed-page output-file)
+      (. Thread (sleep 3000))
+      (when (next-page post-seed-page)
+        (recur (uri/resolve-path
+                post-seed
+                (next-page post-seed-page)) output-file)))))
+
+(defn -main
+  [& args]
+  (let [[optional [post-seeds output-file] banner] (cli/cli args)]
+    (doseq [post-seed (core/process-text-file post-seeds)]
+      (download-post post-seed output-file))))
--- clojure/clueweb12pp_core/src/clueweb12pp_core/utils.clj
+++ clojure/clueweb12pp_core/src/clueweb12pp_core/utils.clj
@@ -85,3 +85,24 @@
 (defn warc-skip-to-record
   [warc-response-records-seq skip-target-uri]
   (drop skip-target-uri warc-response-records-seq))
+
+(defn try-times*
+  "Executes thunk. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n thunk]
+  (loop [n n]
+    (if-let [result (try
+                      [(thunk)]
+                      (catch Exception e
+                        (when (zero? n)
+                          (throw e))))]
+      (result 0)
+      (recur (dec n)))))
+
+(defmacro try-times
+  "Executes body. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n & body]
+  `(try-times* ~n (fn [] ~@body)))
